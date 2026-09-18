f3d8725df95fc2756aa83055796903cdd1c18605
egah <egahepiphane@gmail.com>
2026-09-18 13:24:33 +0200

    fixer le deploiement avec un processus oidc

--- gcr_setup.sh
+++ gcr_setup.sh
@@ -23,4 +23,5 @@ gcloud run deploy jan26-cde-crypto-service \
   --platform=managed \
   --region=europe-west1 \
   --port=8501 \
+  --service-account=github-cloud-run@jan26-cde-crypto.iam.gserviceaccount.com  \
   --allow-unauthenticated
